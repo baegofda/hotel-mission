c1da11917ab0a90d31d4ea923a405d522dab21fa
baegofda <molly.daleo3o@gmail.com>
2023-02-20 16:56:13 +0900

    chore: prettier & eslint 세팅

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  printWidth: 150,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+};

b123c82c47c48b4127b0ec7aef236c2d8faf9a75
baegofda <molly.daleo3o@gmail.com>
2023-02-23 15:05:53 +0900

    feat: useMediaQuery hooks 추가

---
+++ src/hooks/useMediaQuery.ts
@@ -0,0 +1,27 @@
+import { useCallback, useEffect, useState } from 'react';
+import { breakPoints, mediaQuerys } from '@/styles/media';
+
+const useMediaQuery = (breakpoint: (typeof breakPoints)[number]) => {
+  const width = mediaQuerys[breakpoint] ?? 0;
+  const [isRange, setIsRange] = useState(false);
+
+  const handleMedia = useCallback((e: MediaQueryListEvent) => {
+    setIsRange(e.matches);
+  }, []);
+
+  useEffect(() => {
+    if (typeof window === 'undefined') return;
+
+    const media = window.matchMedia(`(min-width:${width}px)`);
+
+    if (media.matches) setIsRange(true);
+
+    media.addEventListener('change', handleMedia);
+
+    return () => media.removeEventListener('change', handleMedia);
+  }, [width, handleMedia]);
+
+  return { isRange };
+};
+
+export default useMediaQuery;

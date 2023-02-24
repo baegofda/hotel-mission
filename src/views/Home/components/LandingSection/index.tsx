c80ebc8dd07efc8de8f218e1a19e1eeda3b8234a
baegofda <molly.daleo3o@gmail.com>
2023-02-24 16:39:27 +0900

    fix: 컴포넌트 구조 변경

---
+++ src/views/Home/components/LandingSection/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import BannerSlider from './BannerSlider';
+import EventBanner from './EventBanner';
+import { useGetHomeData } from '@/api/hooks/main';
+
+const LandingSection = () => {
+  const { data } = useGetHomeData();
+
+  if (!data) return null;
+
+  return (
+    <section>
+      <h2 className="sr-only">호텔 한달살기</h2>
+      <BannerSlider banners={data?.banners} />
+      <EventBanner />
+    </section>
+  );
+};
+
+export default LandingSection;

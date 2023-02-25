b6df0e570961ae6e317c7e86847b022659e3fe93
baegofda <molly.daleo3o@gmail.com>
2023-02-25 16:02:49 +0900

    fix: 슬라이더 동적로딩

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,5 +1,6 @@
+import dynamic from 'next/dynamic';
 import LandingSection from './components/LandingSection';
-import LocalHotelSlider from './components/LocalHotelSlider';
+const LocalHotelSlider = dynamic(() => import('./components/LocalHotelSlider'));
 
 const HomeContainer = () => {
   return (
